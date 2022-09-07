69d6fdb7defceed8cf966a568e193060c1586914
agarciagalan <112832093+agarciagalan@users.noreply.github.com>
2022-09-07 17:03:16 +0200

    Actualización Program.cs
    Versión final que cambia correctamente los nombres de los archivos si existen.

--- Renombrar archivos/Program.cs
+++ Renombrar archivos/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Data;
 using System.Data.SqlClient;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace renombrar_archivos
@@ -13,29 +7,16 @@ namespace renombrar_archivos
     {
         static void Main(string[] args)
         {
-            //lista para guardar las referencias
-            List<string> referencias = new List<string>();
-            //lista para guardar las rutas de los archivos
+            //lista que guarda las rutas
             List<string> rutas = new List<string>();
-
             using (SqlConnection conexion = new SqlConnection())
             {
-                //abrir conexion
-                conexion.ConnectionString = "Server=AKES-DB-01-003;Database=TEMP_IMPORT;Trusted_Connection=true";
+                //se abre la conexion con la base de datos de NOVADB
+                conexion.ConnectionString = "Server=AKES-DB-01-003;Database=NOVADB;Trusted_Connection=true";
                 conexion.Open();
-                //consultar las referencias
-                SqlCommand command = new SqlCommand("SELECT * FROM dbo.[SSD-11064_Renombrar2]", conexion);
+                //se extraen las rutas de los archivos que coinciden con el numero de referencia indicado en el excel
+                SqlCommand command = new SqlCommand("SELECT NHA.ExternFile FROM NOVADB.dbo.niHstAtt AS NHA INNER JOIN NOVADB.dbo.niHist AS NH ON NHA.HistoryNo = NH.HistoryNo INNER JOIN NOVADB.dbo.niAct AS N ON NH.ObjectNo = N.ActNo AND ObjectType = 1 INNER JOIN TEMP_IMPORT.dbo.[SSD-11064_Renombrar2] AS C ON C.Reference = N.ReferenceNo", conexion);
                 using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        //guardar las referencias en la lista referencias
-                        referencias.Add(String.Format("{0}", reader[0]));
-                    }
-                }
-                //consultar las rutas de los archivos
-                SqlCommand command2 = new SqlCommand("SELECT NHA.ExternFile FROM NOVADB.dbo.niHstAtt AS NHA INNER JOIN NOVADB.dbo.niHist AS NH ON NHA.HistoryNo = NH.HistoryNo INNER JOIN NOVADB.dbo.niAct AS N ON NH.ObjectNo = N.ActNo AND ObjectType = 1 INNER JOIN TEMP_IMPORT.dbo.[SSD-11064_Renombrar2] AS C ON C.Reference = N.ReferenceNo", conexion);
-                using (SqlDataReader reader = command2.ExecuteReader())
                 {
                     while (reader.Read())
                     {
@@ -45,16 +26,34 @@ namespace renombrar_archivos
                 conexion.Close();
                 conexion.Dispose();
             }
-            foreach (string referencia in referencias)
+
+            //se recorren las rutas
+            for (int i = 0; i < rutas.Count(); i++)
             {
-                Console.WriteLine(referencia);
-            }
-            foreach (string ruta in rutas)
-            {
-                Console.WriteLine(ruta);
+                //se indica la ruta de los archivos que se estan modificando
+                Console.WriteLine(rutas[i]);
+                string[] referencia = rutas[i].Split('\\');
+                //se extrae la referencia sin los 0
+                int referenciaSinCero = Int32.Parse(referencia[7]);
+                //se extrae la referencia con los 0
+                string referenciaConCero = referencia[7];
+                //se generan las rutas sin 0
+                string rutaCompletaSaldo = rutas[i] + "\\CertSaldo" + referenciaSinCero + ".pdf";
+                string rutaCompletaDispos = rutas[i] + "\\CertDispos" + referenciaSinCero + ".pdf";
+                //se generan las rutas con 0
+                string nuevaRutaCompletaSaldo = rutas[i] + "\\CertSaldo" + referenciaConCero + ".pdf";
+                string nuevaRutaCompletaDispos = rutas[i] + "\\CertDispos" + referenciaConCero + ".pdf";
+                
+                //si existen los ficheros se cambia el nombre por la referencia precedida de 0
+                if (File.Exists(rutaCompletaSaldo))
+                {
+                    System.IO.File.Move(rutaCompletaSaldo, nuevaRutaCompletaSaldo);
+                }
+                if (File.Exists(rutaCompletaDispos))
+                {
+                    System.IO.File.Move(rutaCompletaDispos, nuevaRutaCompletaDispos);
+                }
             }
         }
     }
 }
-
-            
